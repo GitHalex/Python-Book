4b8c8c3dca83629b503a65a5552497f29b29ec80
Hanyinson Alex <calex1731@gmail.com>
2022-07-02 23:17:47 -0400

    Agregando un cuadrado de 100 por 100 en el centro de un cuadrado de 200 por 200

---
+++ cuadradoCien_cuadradoDosientos.py
@@ -0,0 +1,30 @@
+from turtle import Screen, Turtle
+
+pantalla = Screen()
+pantalla.setup(600, 500)
+pantalla.screensize(400, 500)
+
+tortuga = Turtle()
+tortuga.forward(200)
+tortuga.left(90)
+tortuga.forward(200)
+tortuga.left(90)
+tortuga.forward(200)
+tortuga.left(90)
+tortuga.forward(200)
+
+tortuga.penup()
+tortuga.left(135)
+tortuga.forward(70)
+tortuga.pendown()
+
+tortuga.right(45)
+tortuga.forward(100)
+tortuga.left(90)
+tortuga.forward(100)
+tortuga.left(90)
+tortuga.forward(100)
+tortuga.left(90)
+tortuga.forward(100)
+
+pantalla.exitonclick()
